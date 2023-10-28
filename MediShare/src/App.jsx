83530d57d9ec37c5cba78837e49c2bacbad1e60f
Joshua  Mulyadi <jmulyadi1@gmail.com>
2023-10-28 15:47:17 -0400

    changed

--- MediShare/src/App.jsx
+++ MediShare/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import F from "./front.jsx";
 function App() {
   
--- MediShare/src/front.jsx
+++ MediShare/src/front.jsx
@@ -3,24 +3,11 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import { useState } from "react";
 
-const [count, setCount] = useState(0);
+
 export default function Header() {
   return (
     <div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>hey bae</h1>
     </div>
     //hi whats up 
     //Justin is so hawt
