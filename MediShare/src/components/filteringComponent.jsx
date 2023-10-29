3b9e418fabdfac89f326f59a6e5b152a992c8b3d
eisenhauermartin <eisenhauer.s.martin@gmail.com>
2023-10-29 01:54:15 -0400

    filter reset button created

--- MediShare/src/components/filteringComponent.jsx
+++ MediShare/src/components/filteringComponent.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function FilterDropdown() {
+  const [selectedFilter, setSelectedFilter] = useState(''); // Initialize selectedFilter with an empty string
+
+  const handleFilterChange = (event) => {
+    setSelectedFilter(event.target.value);
+  };
+
+  const resetSelection = () => {
+    setSelectedFilter(''); // Reset the selectedFilter to an empty string
+  };
+
   return (
     <div className="filterbar">
-      {/* <label htmlFor="filter">Filter:</label> */}
-      <select className = "filter" id="filter">
+      <select className="filter" id="filter" value={selectedFilter} onChange={handleFilterChange}>
         <option value="" disabled hidden>Select a Filter</option>
         <option value="location">Location</option>
         <option value="specialty">Specialty</option>
         <option value="needs">Needs</option>
       </select>
+      <button onClick={resetSelection}>Reset Selection</button>
     </div>
   );
 }
