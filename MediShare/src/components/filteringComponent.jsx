e6e3529e4b165ad04276475b33ab24901952d654
eisenhauermartin <eisenhauer.s.martin@gmail.com>
2023-10-29 01:20:20 -0400

    filterbar centered and fixed visual

--- MediShare/src/components/filteringComponent.jsx
+++ MediShare/src/components/filteringComponent.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 function FilterDropdown() {
   return (
     <div className="filterbar">
-      <label htmlFor="filter">Filter:</label>
+      {/* <label htmlFor="filter">Filter:</label> */}
       <select className = "filter" id="filter">
+        <option value="" disabled hidden>Select a Filter</option>
         <option value="location">Location</option>
         <option value="specialty">Specialty</option>
         <option value="needs">Needs</option>
