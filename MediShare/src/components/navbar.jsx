a33c9333812b81d3182e0fe6203c04872d0710b3
eisenhauermartin <eisenhauer.s.martin@gmail.com>
2023-10-29 00:21:58 -0400

    Navbar created

--- MediShare/src/components/navbar.jsx
+++ MediShare/src/components/navbar.jsx
@@ -4,10 +4,13 @@ import { useState } from "react";
 
 export default function navbar() {
   return (
-    <div>
-      <h1>hey bae</h1>
-    </div>
-    //hi whats up 
-    //Justin is so hawt
-  );
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item"><a href="/">Home</a></li>
+        <li className="nav-item"><a href="/about">About</a></li>
+        <li className="nav-item"><a href="/services">Services</a></li>
+        <li className="nav-item"><a href="/contact">Contact</a></li>
+      </ul>
+    </nav>
+  )
 }
