700770ec3fc29578c9872f61a13864607b22f0b8
Joshua  Mulyadi <jmulyadi1@gmail.com>
2023-10-29 01:34:35 -0400

    bigger

--- MediShare/src/components/boxes.jsx
+++ MediShare/src/components/boxes.jsx
@@ -1,35 +1,41 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import profiles from "../assets/data.js"
-import db from "./firebase.js"
+import profiles from "../assets/data.js";
+import db from "./firebase.js";
 import { onSnapshot, collection } from "firebase/firestore";
-
 
 export default function boxes() {
   const [info, setInfo] = useState([]);
 
   console.log(info);
 
-  useEffect(() =>
-    onSnapshot(collection(db, "DoctorInfo"), (snapshot) =>
-      setInfo(snapshot.docs.map(doc => doc.data()))
-    ), []);
+  useEffect(
+    () =>
+      onSnapshot(collection(db, "DoctorInfo"), (snapshot) =>
+        setInfo(snapshot.docs.map((doc) => doc.data()))
+      ),
+    []
+  );
 
   return (
     <div className="profile-grid">
       {profiles.map((profile, index) => (
         <div key={index} className="profile-box">
-          <img className="profile-image" src={profile.imageUrl} alt={profile.name} />
+          <a href="https://www.example.com">
+            <img key = {index}
+              className="profile-image"
+              src={profile.imageUrl}
+              alt={profile.name}
+            />
+          </a>
           <h3 className="profile-name">{profile.name}</h3>
           <p className=".profile-description">{profile.description}</p>
         </div>
       ))}
-      
+
       <ul>
-        {info.map(i => (
-          <p>
-            {i.Name}
-          </p>
+        {info.map((i) => (
+          <p>{i.Name}</p>
         ))}
       </ul>
     </div>
