c81a280b9e970071e706c9b7a30f792546fab33e
justinhylee135 <justinhylee135@gmail.com>
2023-10-29 00:33:49 -0400

    frick

--- MediShare/src/components/boxes.jsx
+++ MediShare/src/components/boxes.jsx
@@ -3,10 +3,29 @@ import { useState } from "react";
 
 
 export default function boxes() {
+  // const [allDocs, setAllDocs] = useState([]);
+  // const db = firebase.firestore();
+
+  // function fetchAll(e) {
+  //   e.preventDefault();
+  //   db.collection("DoctorInfo")
+  //   .get()
+  //   .then((snapshot)=>{
+  //     if(snapshot.docs.length > 0){
+  //       snapshot.docs.forEach((doc) => {
+  //         setAllDocs((prev) => {
+  //           return[...prev, doc.data()];
+  //         })
+  //       })
+  //     }
+  //   })
+  // }
+
+
   return (
     //justin
     <div>
-      <h1>Cock </h1>
+      <h1>piss is stored in the balls</h1>
     </div>
     //hi whats up 
     //Justin is so hawt
