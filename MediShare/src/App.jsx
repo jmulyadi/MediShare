612a112c495f00d380b46fc96d99e21140a3b837
Joshua  Mulyadi <jmulyadi1@gmail.com>
2023-10-29 02:16:32 -0400

    commiting

--- MediShare/src/App.jsx
+++ MediShare/src/App.jsx
@@ -1,14 +1,31 @@
 import Navbar from "./components/navbar.jsx";
 import Boxes from "./components/boxes.jsx";
 import Filter from "./components/filteringComponent.jsx";
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Page from "./components/page.jsx"
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Filter />
+        <Boxes />
+      </div>
+    ),
+  },{
+    path: "/other",
+    element: (
+      <div>
+        <Page />
+      </div>
+    ),
+  },
+]);
 function App() {
-  
   return (
     <>
-      <Navbar />
-      <Filter />
-      <Boxes />
+      <RouterProvider router={router} />
     </>
   );
 }
--- MediShare/src/components/page.jsx
+++ MediShare/src/components/page.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import profiles from "../assets/data.js";
+import Navbar from "./navbar.jsx";
 function Page() {
   return (
     <div>
-      <h2>{profiles[0]}</h2>
+      <Navbar />
+      {profiles
+        .filter((profile, index) => index === 0) // Filter the first item
+        .map((profile, index) => (
+          <div key={index} className="profile-box">
+            <a href="/other">
+              <img
+                key={index}
+                className="profile-image"
+                src={profile.imageUrl}
+                alt={profile.name}
+              />
+            </a>
+            <h3 className="profile-name">{profile.name}</h3>
+            <p className="profile-description">{profile.description}</p>
+          </div>
+        ))}
     </div>
   );
 }
