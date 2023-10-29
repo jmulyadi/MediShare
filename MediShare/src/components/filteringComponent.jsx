5d38cef3945dc3c2aeabcd6237cde45e2ca40a6c
eisenhauermartin <eisenhauer.s.martin@gmail.com>
2023-10-29 00:51:36 -0400

    filteringComponent changes

--- MediShare/src/components/filteringComponent.jsx
+++ MediShare/src/components/filteringComponent.jsx
@@ -4,13 +4,13 @@ function FilteringComponent({ selectedFilter, onFilterChange, filterOptions }) {
   return (
     <div className="filtering-component">
       <label>Select a Filter:</label>
-      <select value={selectedFilter} onChange={onFilterChange}>
+      {/* <select value={selectedFilter} onChange={onFilterChange}>
         {filterOptions.map((option) => (
           <option key={option} value={option}>
             {option}
           </option>
         ))}
-      </select>
+      </select> */}
     </div>
   );
 }
