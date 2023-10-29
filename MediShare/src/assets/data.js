2eeffe588538b102f4e9c3f070fb2c3ca0ef4549
Joshua  Mulyadi <jmulyadi1@gmail.com>
2023-10-28 23:31:36 -0400

    stuff

---
+++ MediShare/src/assets/data.js
@@ -0,0 +1,34 @@
+
+    const profiles = [
+      {
+        name: 'User 1',
+        imageUrl: 'url_to_image_1.jpg',
+        description: 'Description for User 1',
+      },
+      {
+        name: 'User 2',
+        imageUrl: 'url_to_image_2.jpg',
+        description: 'Description for User 2',
+      },
+      {
+        name: 'User 3',
+        imageUrl: 'url_to_image_3.jpg',
+        description: 'Description for User 3',
+      },
+      {
+        name: 'User 4',
+        imageUrl: 'url_to_image_4.jpg',
+        description: 'Description for User 4',
+      },
+      {
+        name: 'User 5',
+        imageUrl: 'url_to_image_5.jpg',
+        description: 'Description for User 5',
+      },
+      {
+        name: 'User 6',
+        imageUrl: 'url_to_image_6.jpg',
+        description: 'Description for User 6',
+      },
+    ];
+  export default profiles;
--- MediShare/src/components/boxes.jsx
+++ MediShare/src/components/boxes.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { useState } from "react";
-
+import profiles from "../assets/data.js"
 
 export default function boxes() {
   return (
-    //justin
-    <div>
-      <h1>Cock </h1>
+    <div className="profile-grid">
+      {profiles.map((profile, index) => (
+        <div key={index} className="profile-box">
+          <img src={profile.imageUrl} alt={profile.name} />
+          <h3>{profile.name}</h3>
+          <p>{profile.description}</p>
+        </div>
+      ))}
     </div>
-    //hi whats up 
-    //Justin is so hawt
   );
 }
